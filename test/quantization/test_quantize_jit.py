f6461745c00343061385f48e433144b3decd665a
vasiliy <vasiliy@fb.com>
2020-06-30 12:12:15 -0700

    [wip] qat graph mode: add combine_conv_bn pass
    Summary:
    
    not ready for review yet.
    
    Adds the JIT pass to combine conv and bn before training.
    
    Test Plan:
    
    PYTORCH_JIT_LOG_LEVEL=">qat_combine_conv_bn" python test/test_quantization.py TestQuantizeQATScript
    
    python test/test_quantization.py TestQATModule
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 9c8ff6209e97802e51c66366cdfcb29aef9de515
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39828

--- test/quantization/test_quantize_jit.py
+++ test/quantization/test_quantize_jit.py
@@ -2951,25 +2951,51 @@ class TestQuantizeJit(QuantizationTestCase):
 class TestQuantizeQATJit(QuantizationTestCase):
 
     def test_prepare_qat(self):
+
         class M(torch.nn.Module):
             def __init__(self):
                 super(M, self).__init__()
-                self.conv = torch.nn.Conv2d(1, 1, 1)
-                self.bn = torch.nn.BatchNorm2d(1)
+                self.conv11 = torch.nn.Conv2d(1, 1, 1)
+                self.bn11 = torch.nn.BatchNorm2d(1)
 
             def forward(self, x):
-                x = self.conv(x)
-                x = self.bn(x)
+                x = self.conv11(x)
+                x = self.bn11(x)
                 return x
+
+        if False:
+            # debug only - eager mode
+            eager = M()
+            torch.quantization.fuse_modules(eager, [['conv11', 'bn11']], inplace=True)
+            eager.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm')
+            torch.quantization.prepare_qat(eager, inplace=True)
+            # print(eager)
+            eager_scripted = torch.jit.script(eager)
+            # print(eager_scripted)
+            # print(eager_scripted.conv1._forward.graph)
+            # print(type(eager_scripted.conv1._forward.graph))
+            # print(eager_scripted.conv1._forward.code)
+            # scripted_func = torch.jit.script(torch.nn.intrinsic.qat.modules.conv_fused._conv_forward_and_unscale_2)
+            # print(scripted_func.graph)
+            # print(scripted_func.code)
+            # print(eager_scripted.conv1._conv_forward_and_unscale_2.graph)
+            # print(eager_scripted.conv1._conv_forward_and_unscale.code)
 
         m = torch.jit.script(M())
         m = prepare_qat_jit(m, {'': default_qat_qconfig})
 
+        # run an image through
+        # TODO (next): fix the error (most likely need to fix the types on self.conv and self.bn in the IR)
+        data = torch.rand(4, 1, 1, 1)
+        result = m(data)
+        print(result)
+
         # TODO(future PR): modify this as needed after we add QAT conv-bn logic
-        assert len(attrs_with_prefix(m, '_observer_')) == 2
-        assert len(attrs_with_prefix(m.conv, '_observer_')) == 1
-        FileCheck().check('FakeQuantize = prim::GetAttr[name="_observer_') \
-                   .check('prim::GetAttr[name="conv"]') \
-                   .check('prim::CallMethod') \
-                   .check_not('Observer = prim::GetAttr[name="_observer_') \
-                   .run(m.graph)
+        if False:
+            assert len(attrs_with_prefix(m, '_observer_')) == 2
+            assert len(attrs_with_prefix(m.conv1, '_observer_')) == 1
+            FileCheck().check('FakeQuantize = prim::GetAttr[name="_observer_') \
+                       .check('prim::GetAttr[name="conv1"]') \
+                       .check('prim::CallMethod') \
+                       .check_not('Observer = prim::GetAttr[name="_observer_') \
+                       .run(m.graph)
